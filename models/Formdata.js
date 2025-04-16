0184fa0f29d77db92f65d42ccb02cf0ef48e649a
Pramit Bakhrel <66194484+EmmittRel@users.noreply.github.com>
2025-04-17 00:58:35 +0545

    Update Formdata.js

--- models/Formdata.js
+++ models/Formdata.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const formDataSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    message: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true });
+// const formDataSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true
+//     },
+//     message: {
+//         type: String,
+//         required: true
+//     }
+// }, { timestamps: true });
 
-module.exports = mongoose.model('FormData', formDataSchema);
+// module.exports = mongoose.model('FormData', formDataSchema);
